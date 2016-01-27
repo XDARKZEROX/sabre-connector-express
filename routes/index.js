b221950e2b47ce94c8d9df01123bb8a62794f128
Alexander Guzman <a.guzman@costamar.com>
2016-01-27 15:46:56 -0500

    Se agregaron los paquetes node-soap y xml2js via npm y se agrego un test usando la web service del clima (webservicex.com/globalweather)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
-/* GET home page. */
+//No olvidar instalar estas dependencias
+var soap = require('soap');
+var parseString = require('xml2js').parseString;
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //http://www.webservicex.com/globalweather.asmx?WSDL
+  var url = 'http://www.webservicex.com/globalweather.asmx?WSDL';
+  var args = {CityName: 'Lima', CountryName: 'PE' };
+  soap.createClient(url, function(err, client) {
+	client.GetWeather(args, function(err, result) {
+		parseString(result.GetWeatherResult, function (err, result) {
+    		res.json(result);
+		});
+		});
+	});
 });
 
 module.exports = router;
+
+
+
