804e1dc590054f75fd0e044f211e8b7998764737
Alexander Guzman <a.guzman@costamar.com>
2016-02-18 17:00:50 -0500

    Se agregaron constantes de authenticate y se agrego el package de unit testing con la libreria mocha

---
+++ test/auth-test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var fs = require('fs'),
+    http = require('http'),
+    assert = require('assert');
+
+describe('Sabre Sessions', function() {
+
+	it('should show any message', function(done) {
+		var err = "prueba";
+		//assert.ok(err);
+		assert.notStrictEqual(err, null)
+	    done();
+	});
+});    
