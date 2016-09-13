4785bd157d658f1304ed3dde0c22e8f937dfc828
Alexander <a.guzman@costamar.com>
2016-09-13 18:09:40 -0500

    Agregada primera version de  util para invocar al servicio rest de sabre. Terminado el armado del request para bargainFinderMax

--- app/controller/search.js
+++ app/controller/search.js
@@ -7,6 +7,7 @@ var express = require('express'),
     async = require('async'),
     parseString = require('xml2js').parseString,
     auth = require('../../lib/helpers/auth'),
+    rest = require('../../lib/helpers/rest'),
     searchBuilder = require('../../lib/builder/searchBuilder');
 var logger = require('../../config/logger');
 module.exports = {
@@ -22,6 +23,8 @@ module.exports = {
                 blockedAirlines['blocked_airlines'].splice(index, 1);
             }
         });
+
+        var request;
         var sabre_session;
         async.series([
             //Obtenemos el token de sesion (falta validarlo dependiendo del status)
@@ -33,8 +36,23 @@ module.exports = {
             },
             //procedemos a armar el request y luego lo enviamos a Sabre
             function(callback){
-                searchBuilder.buildBargainFinderMax(airAvailRQ , blockedAirlines);
+                var sabreReq = searchBuilder.buildBargainFinderMax(airAvailRQ , blockedAirlines);
+
+                request = {
+                    event : "bfm",
+                    nextEvent : "end",
+                    token : sabre_session,
+                    service : "/v1.9.4/shop/flights?mode=live",
+                    query: sabreReq
+                }
+
+                rest.post(request)
+
                 callback();
+            },
+            function(callback){
+
+                console.log("llamada al servicio web de Sabre")
             }
         ],function(err) {
             //console.log('second');
